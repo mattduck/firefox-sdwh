90d062edf9e5e11fda58ea4395281644d25da5bd
Matt Duck <matt@mattduck.com>
2023-08-05 16:17:14 +0100

    fix: properly support selections spanning multiple elements
    This is something that has annoyed me since I first wrote the
    extension 3 years ago, but not enough that I've actually looked
    into it until now. The problem was that the implementation
    was very basic - it just wrapped your selected text in a span element,
    and that was it. If your selection covered text that spanned multiple
    HTML elements, then it wouldn't work. The fix I've gone with is
    basically do the same "wrap-in-a-span" approach, but do it to
    all the elements that you've highlighted individually.

--- content.js
+++ content.js
@@ -1,54 +1,160 @@
 /*
-IDEAS:
-- [ ] Keep selections after page refresh
-- [ ] Remove selections
-- [ ] Fix allowing multiple highlights on same thing
-- [ ] Fix issue where things not always highlighted
+  IDEAS:
+  - [ ] Remove selections
+  - [ ] Fix allowing multiple highlights on same thing
+  - [ ] Keep selections after page refresh
 */
 
-const buildKey = (text) => {
-    const key = window.location.toString() + text;
-    return key
+// Return unique-ish ID. Used to group all the spans that were highlighted together.
+const generateId = () => {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2, 10);
 }
 
-const copyAll = () => {
-    const spans = document.querySelectorAll(".matt-highlight");
+// Create the span element that will wrap the text to be highlighted
+const createHighlightWrapper = (uniqueId) => {
+    const wrapper = document.createElement('span');
+    wrapper.style.backgroundColor = "#EEEE00";
+    wrapper.style.color = "#111111";
+    wrapper.classList.add("sdwh-highlight");
+    wrapper.setAttribute("sdwh-highlight-id", uniqueId);
+    wrapper.style.fontSize = "1.1em";
+    return wrapper;
+};
+
+// Helpers to traverse the DOM, which is something we have to do if
+// a selection spans multiple elements.
+const getNextNode = (node) => {
+    if (node.nextSibling) {
+        return getDeepFirstChild(node.nextSibling);
+    }
+    while (node.parentNode && !node.parentNode.nextSibling) {
+        node = node.parentNode;
+    }
+    return node.parentNode ? getDeepFirstChild(node.parentNode.nextSibling) : null;
+};
+const getDeepFirstChild = (node) => {
+    while (node && node.firstChild) {
+        node = node.firstChild;
+    }
+    return node;
+};
+
+// How we detect whether the user can "see" this item. Used to fix issue
+// where sometimes extra text could appear in the clipboard output, because
+// it was part of an html element that wasn't actually visible.
+const isVisible = (node) => {
+    const style = window.getComputedStyle(node.parentNode, null);
+    return style.display !== "none";
+};
+
+// Detect if an element overlaps with the given selection range. Used as part
+// of detecting whether to continue traversing the DOM.
+const isAnyPartOfElementInsideRange = (element, range) => {
+    const elemRange = document.createRange();
+    elemRange.selectNode(element);
+    const startsBeforeRangeEnds = range.compareBoundaryPoints(Range.END_TO_START, elemRange) <= 0;
+    const endsAfterRangeStarts = range.compareBoundaryPoints(Range.START_TO_END, elemRange) >= 0;
+    return startsBeforeRangeEnds && endsAfterRangeStarts;
+}
+
+// Entrypoint for the highlight action. If the highlighted text is all contained
+// in one span, then it's easy - we just wrap that text in a new span with our
+// "highlight" class. If the selected text is split across multiple HTML
+// elements (which is fairly likely), then we have to run the same process
+// individually on all those elements, and figure out when to stop traversing
+// the DOM.
+const commandHighlightSelection = () => {
+    const sel = document.getSelection();
+    if (!sel.rangeCount) return;
+
+    for (let i = 0; i < sel.rangeCount; i++) {
+        const range = sel.getRangeAt(i);
+        const uniqueId = generateId();
+
+        if (range.startContainer === range.endContainer) {
+            const wrapper = createHighlightWrapper(uniqueId);
+            range.surroundContents(wrapper);
+            continue;
+        }
+
+        let currentNode = range.startContainer;
+        const nodesToWrap = [];
+        while (currentNode) {
+            if (!isAnyPartOfElementInsideRange(currentNode, range)){
+                break;
+            }
+            if (currentNode.nodeType === Node.TEXT_NODE && currentNode.textContent.trim() !== "" && isVisible(currentNode)) {
+                nodesToWrap.push(currentNode);
+            }
+            currentNode = currentNode === range.endContainer ? null : getNextNode(currentNode);
+        }
+
+        nodesToWrap.forEach(node => {
+            const wrapperRange = document.createRange();
+            if (node === range.startContainer) {
+                wrapperRange.setStart(node, range.startOffset);
+                wrapperRange.setEnd(node, node.length);
+            } else if (node === range.endContainer) {
+                wrapperRange.setStart(node, 0);
+                wrapperRange.setEnd(node, range.endOffset);
+            } else {
+                wrapperRange.selectNodeContents(node);
+            }
+            const wrapper = createHighlightWrapper(uniqueId);
+            wrapperRange.surroundContents(wrapper);
+        });
+    }
+};
+
+
+// Entrypoint for copy action. We mostly just store the highlighted text
+// content, with a separator to represent each individual highlight.
+// We do very basic formatting of the plain text - eg. to make sure
+// we put headings on a newline. But mostly it's just copied as it
+// is in the HTML.
+const commandCopyAll = () => {
+    const spans = document.querySelectorAll(".sdwh-highlight");
     var s = window.location.toString();
+    var currentId;
     for (var i=0; i < spans.length; i++) {
-        s += "\n\n";
-        s += "------------------------------------------------------------";
-        s += "\n";
+        var thisId = spans[i].getAttribute("sdwh-highlight-id");
+        if (thisId != currentId) {
+            // We're now on a new highlight, so store this text separately.
+            s += "\n\n";
+            s += "----------";
+            s += "\n";
+        }
+        currentId = thisId;
         s += spans[i].textContent.trim();
+
+        parent = spans[i].parentNode;
+        if (parent && ["h1", "h2", "h3", "h4", "h5"].includes(parent.tagName.toLowerCase())) {
+            s += "\n\n"
+        } else {
+            s += " "
+        }
     }
     navigator.clipboard.writeText(s);
     alert("Copied highlights");
 }
 
-const highlightSelection = () => {
-    const sel = document.getSelection();
-    const range = sel.getRangeAt(0);
-    const wrapper = document.createElement("span");
-    wrapper.style.backgroundColor = "#EEEE00";
-    wrapper.style.color = "#111111";
-    wrapper.classList.add("matt-highlight");
-    wrapper.style.fontSize = "1.2em";
-    range.surroundContents(wrapper);
-}
 
+// Handles messages from the context menu.
 const handle = (message) => {
     if (message == "highlight") {
-        highlightSelection();
+        commandHighlightSelection();
     } else if (message == "copy") {
-        copyAll();
+        commandCopyAll();
     }
 }
 browser.runtime.onMessage.addListener(handle);
 
+// Listen for our hard-coded keypresses and run the appropriate actions.
 document.addEventListener('keydown', (event) => {
     if (event.ctrlKey && event.key == "L") {
-        highlightSelection();
+        commandHighlightSelection();
     }
     if (event.ctrlKey && event.key == ":") {
-        copyAll();
+        commandCopyAll();
     }
 }, false);
